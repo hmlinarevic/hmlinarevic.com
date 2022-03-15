09affd7df822c702f6940bb4f150c8937d71e5b9
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-15 20:48:55 +0100

    feat: change cards display to flex

--- pages/julie.js
+++ pages/julie.js
@@ -6,56 +6,44 @@ import { getJuliesData } from '../lib/cms/julie'
 import { DOMAIN } from '../utils/url/assets'
 
 export default function Julie({ intros, photos }) {
-  const juliesPhotos = photos.map((photo) => {
-    return DOMAIN + photo.attributes.url
-  })
+  const juliesPhotos = photos.map((photo) => DOMAIN + photo.attributes.url)
 
   return (
     <>
       <h1 className="text-4xl font-bold text-center mb-20">Julie</h1>
 
-      <div className="grid grid-cols-2 gap-x-10 mb-10 items-center border pr-10 rounded-2xl bg-neutral-50 shadow">
+      <div className="bg-neutral-100 border-r-2 rounded-xl pr-10 shadow-xl flex justify-between items-center">
         <Image
-          className="rounded-l-2xl"
+          className="rounded-l-xl"
           src={juliesPhotos[0]}
-          width={400}
-          height={400}
+          width={300}
+          height={300}
           objectFit="cover"
-          objectPosition="50% 50%"
         />
 
-        <Text
-          className=" text-lg basis-80 font-light leading-relaxed"
-          data={intros[0]}
-        />
+        <Text className="basis-80 text-lg leading-relaxed" data={intros[0]} />
       </div>
 
-      <div className="grid grid-cols-2 gap-x-10 mb-10 items-center border rounded-2xl pl-10 bg-neutral-50 shadow">
-        <Text
-          className="text-lg font-light leading-relaxed "
-          data={intros[1]}
-        />
+      <div className="bg-neutral-100 border-l-2 rounded-xl pl-10 my-12 shadow-xl flex justify-between items-center">
+        <Text className="basis-80 text-lg leading-relaxed" data={intros[1]} />
         <Image
-          className="rounded-r-2xl"
+          className="rounded-r-xl"
           src={juliesPhotos[1]}
-          width={400}
-          height={300}
+          width={300}
+          height={220}
           objectFit="cover"
         />
       </div>
 
-      <div className="grid grid-cols-2 gap-x-10 mb-10 items-center border rounded-2xl bg-neutral-50 shadow">
+      <div className="bg-neutral-100  border-r-2 rounded-xl pr-10 shadow-xl flex justify-between items-center">
         <Image
-          className="rounded-l-2xl"
+          className="rounded-l-xl"
           src={juliesPhotos[2]}
-          width={400}
-          height={300}
+          width={300}
+          height={220}
           objectFit="cover"
         />
-        <Text
-          className="text-lg font-light leading-relaxed "
-          data={intros[2]}
-        />
+        <Text className="basis-80 text-lg leading-relaxed" data={intros[2]} />
       </div>
     </>
   )
