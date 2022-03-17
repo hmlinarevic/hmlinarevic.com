14b5d33d047bce432531544492a0260ab0d32699
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-17 19:15:39 +0100

    feat: add content

--- pages/me.js
+++ pages/me.js
@@ -1,26 +1,47 @@
+import Link from 'next/link'
+
 import ReactMarkdown from 'react-markdown'
 
-const Me = ({ data }) => {
-	return (
-		<div>
-			<h1 className="text-4xl font-bold text-center mb-8">about me</h1>
-			<ReactMarkdown className="text-xl font-light leading-relaxed">
-				{data}
-			</ReactMarkdown>
-		</div>
-	)
+import Intro from '../components/intro'
+import Section from '../components/section'
+
+import { getMyData } from '../lib/cms/me'
+
+const Me = ({ about }) => {
+  return (
+    <>
+      <Intro title="about me">
+        Tall, strong, blonde and beautiful with blue eyes - as one Croatian
+        comedian would say when talking about himself on stage. In reality, he
+        is short and chubby but he's got charisma.
+      </Intro>
+      <hr className="w-[60px] mx-auto mt-[-2.5rem] mb-12" />
+
+      <ReactMarkdown className="text-xl leading-relaxed">{about}</ReactMarkdown>
+
+      <p className="text-xl leading-relaxed mt-5">
+        If you want to know more, check out this{' '}
+        <Link href="/blog/shoulders-of-giants">
+          <a className="underline decoration-[#619B60] underline-offset-2 hover:bg-[#619B60] hover:text-[#fff]">
+            article
+          </a>
+        </Link>
+        .
+      </p>
+
+      <Section className="mt-24" title="Courses"></Section>
+    </>
+  )
 }
 
 export default Me
 
 export async function getStaticProps() {
-	const res = await fetch('https://strapi.hmlinarevic.com/api/hrvoje')
-	const data = await res.json()
-	const aboutMeText = data.data.attributes.about_me
+  const { about } = await getMyData()
 
-	return {
-		props: {
-			data: aboutMeText,
-		},
-	}
+  return {
+    props: {
+      about,
+    },
+  }
 }
