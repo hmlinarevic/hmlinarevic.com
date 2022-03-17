c1800568f5b8b9e32137d0ff365229a0de8db0ac
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-17 19:16:14 +0100

    feat: add component

---
+++ components/intro.js
@@ -0,0 +1,17 @@
+export default function Intro({ title, children }) {
+  return (
+    <>
+      <section className="text-center mb-20 ">
+        <h1 className="text-4xl font-bold mb-4">
+          {/* <span className="text-neutral-700">{title[0]}</span> */}
+          {title[0].toUpperCase()}
+          {title.slice(1)}
+        </h1>
+
+        <p className="text-neutral-500 text-2xl leading-relaxed font-serif">
+          {children}
+        </p>
+      </section>
+    </>
+  )
+}
---
+++ components/title.js
@@ -0,0 +1,3 @@
+export default function Title({ children }) {
+  return <h1 className="text-4xl font-bold text-center mb-10">{children}</h1>
+}
