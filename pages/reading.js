d72a2edf3b0bb02cd1839ef9a09620bbfef40a78
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-18 18:07:42 +0100

    feat: updated page components

--- pages/reading.js
+++ pages/reading.js
@@ -1,56 +1,32 @@
 import Bookshelf from '../components/bookshelf/bookshelf'
+import Intro from '../components/intro'
+import Title from '../components/title'
 
-const Reading = ({ csAndWebDev, selfImprovement, novels }) => {
-	return (
-		<>
-			<Bookshelf
-				name="Computer Science & Web Development"
-				books={csAndWebDev}
-			/>
+import { getBooksData } from '../lib/cms/books'
 
-			<Bookshelf name="Self Improvement" books={selfImprovement} />
+export default function Reading({ books }) {
+  return (
+    <>
+      <Intro title="reading">Books I'm reading and some of my favorites</Intro>
 
-			<Bookshelf name="Novels" books={novels} />
-		</>
-	)
+      <Bookshelf
+        name="Computer Science & Web Development"
+        books={books.csAndWebDev}
+      />
+
+      <Bookshelf name="Personal Development" books={books.selfImprovement} />
+
+      <Bookshelf name="Novels" books={books.novels} />
+    </>
+  )
 }
 
 export async function getStaticProps() {
-	const res = await fetch('https://strapi.hmlinarevic.com/api/books?populate=*')
+  const books = await getBooksData()
 
-	const { data } = await res.json()
-
-	const books = data.map((book) => {
-		const { id } = book
-		const { name, author, status, url, category } = book.attributes
-		const coverUrl = book.attributes.cover.data.attributes.url
-
-		return {
-			id,
-			name,
-			author,
-			category,
-			coverUrl,
-			url,
-			status,
-		}
-	})
-
-	const csAndWebDev = books.filter(
-		(book) => book.category === 'computer science & web development'
-	)
-	const selfImprovement = books.filter(
-		(book) => book.category === 'self improvement'
-	)
-	const novel = books.filter((book) => book.category === 'novel')
-
-	return {
-		props: {
-			csAndWebDev,
-			selfImprovement,
-			novels: novel,
-		},
-	}
+  return {
+    props: {
+      books,
+    },
+  }
 }
-
-export default Reading
