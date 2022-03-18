e24791effe2015d1af9b6d46af314e91f9a5829d
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-18 18:02:03 +0100

    fix: update max width in the section title

--- components/bookshelf/bookshelf.js
+++ components/bookshelf/bookshelf.js
@@ -2,23 +2,21 @@ import Shelf from './shelf'
 import Book from './book'
 
 const Bookshelf = ({ name, books }) => {
-	console.log(books)
+  return (
+    <section className="mb-24">
+      <h2 className="text-2xl font-bold max-w-[250px]">{name}</h2>
+      <hr className="mt-2 mb-8" />
 
-	return (
-		<section className="mb-24">
-			<h2 className="text-2xl font-bold max-w-[240px]">{name}</h2>
-			<hr className="mt-2 mb-8" />
-
-			<Shelf
-				name="Currently Reading"
-				books={books.filter((book) => book.status === 'reading')}
-			/>
-			<Shelf
-				name="All-time Favorites"
-				books={books.filter((book) => book.status === 'favorite')}
-			/>
-		</section>
-	)
+      <Shelf
+        name="Currently Reading"
+        books={books.filter((book) => book.status === 'reading')}
+      />
+      <Shelf
+        name="All-time Favorites"
+        books={books.filter((book) => book.status === 'favorite')}
+      />
+    </section>
+  )
 }
 
 export default Bookshelf
