5bd2bbf856d9013ebeedb52766e5669f4cbc21ab
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-18 18:05:17 +0100

    feat: updated page components

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,39 +1,40 @@
 import ReactMarkdown from 'react-markdown'
 
 import Date from '../../components/date'
+import Text from '../../components/text'
 
-import { getAllArticleSlugs, getArticleData } from '../../lib/articles'
+import { getAllArticleSlugs, getArticleData } from '../../lib/cms/articles'
 
 export const getStaticPaths = async () => {
-	const paths = await getAllArticleSlugs()
-	return {
-		paths,
-		fallback: false,
-	}
+  const paths = await getAllArticleSlugs()
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export const getStaticProps = async ({ params }) => {
-	const articleData = await getArticleData(params.slug)
-	return {
-		props: {
-			articleData,
-			params,
-		},
-	}
+  const articleData = await getArticleData(params.slug)
+  return {
+    props: {
+      articleData,
+      params,
+    },
+  }
 }
 
-export default function Blog({ articleData }) {
-	const { content, title, publishedAt } = articleData.attributes
-	console.log(articleData)
-	return (
-		<>
-			<span className="font-light block text-center mb-4">
-				Written by <span className="font-bold">Hrvoje Mlinarevic</span> on{' '}
-				<Date dateString={publishedAt} />
-			</span>
-			<h1 className="text-4xl font-bold text-center">{title}</h1>
-			<hr className="w-[120px] my-10 mx-auto" />
-			<ReactMarkdown className="font-serif">{content}</ReactMarkdown>
-		</>
-	)
+export default function Article({ articleData }) {
+  const { content, title, publishedAt } = articleData.attributes
+  console.log(articleData)
+  return (
+    <>
+      <span className="font-light block text-center mb-4">
+        Written by <span className="font-bold">Hrvoje Mlinarevic</span> on{' '}
+        <Date dateString={publishedAt} formatString="LLLL d, yyyy" />
+      </span>
+      <h1 className="text-4xl font-bold text-center">{title}</h1>
+      <hr className="w-[120px] my-10 mx-auto" />
+      <Text className="font-serif text-lg" data={content} />
+    </>
+  )
 }
