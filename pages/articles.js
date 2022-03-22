8bff199f9ee9f2ea6b6746467913b73d17034f8d
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-22 19:01:35 +0100

    fix: delete

--- pages/articles.js
+++
@@ -1,5 +0,0 @@
-const Articles = () => {
-	return <h1 className="text-3xl font-bold text-center">Articles</h1>
-}
-
-export default Articles
--- utils/hostAddress.js
+++
@@ -1,1 +0,0 @@
-export const hostAddress = 'https://strapi.hmlinarevic.com'
