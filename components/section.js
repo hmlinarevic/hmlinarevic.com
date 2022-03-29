cbf3af83a7d1edc2ada7c26489035bda39d7321e
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-29 18:50:52 +0200

    feat: updated margin bottom

--- components/section.js
+++ components/section.js
@@ -1,11 +1,11 @@
 const Section = ({ className, title, children }) => {
-	return (
-		<section className={className}>
-			{title && <h2 className="text-3xl font-bold">{title}</h2>}
-			{title && <hr className="mt-2 mb-4" />}
-			{children}
-		</section>
-	)
+  return (
+    <section className={className}>
+      {title && <h2 className="text-3xl font-bold ">{title}</h2>}
+      {title && <hr className="mt-2 mb-8" />}
+      {children}
+    </section>
+  )
 }
 
 export default Section
