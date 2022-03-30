70f6c27fa27547efba05b97cba421803fdaaffc2
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-03-30 18:34:02 +0200

    fix: update h2 max width

--- components/section.js
+++ components/section.js
@@ -1,7 +1,7 @@
 const Section = ({ className, title, children }) => {
   return (
     <section className={className}>
-      {title && <h2 className="text-3xl font-bold ">{title}</h2>}
+      {title && <h2 className="text-3xl font-bold max-w-[310px]">{title}</h2>}
       {title && <hr className="mt-2 mb-8" />}
       {children}
     </section>
