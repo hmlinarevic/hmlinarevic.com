dd24288d425a606bd38057cc3c0a1a71c9d18562
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-17 23:45:07 +0200

    feat: add projects section

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ import ArticleList from '../components/article/article-list'
 import { getMyData } from '../lib/cms/me'
 import { getSiteData } from '../lib/cms/site'
 import { getArticlesData } from '../lib/cms/articles'
+import Image from 'next/image'
+
+import hexagonSvg from '../public/hexagon.svg'
 
 export default function Home({ me, site, articles }) {
   return (
@@ -16,6 +19,19 @@ export default function Home({ me, site, articles }) {
 
       <Section className="mt-24" title="A Bit About the Site">
         <Text className="font-serif text-lg" data={site.about} />
+      </Section>
+
+      <Section className="mt-24" title="Projects">
+        <Text className="font-serif text-lg" />
+
+        <a href="https://www.crosswit.io">
+          <div className="w-[calc(100%+40px)] ml-[-20px]">
+            <div className="mt-10 py-[10px] px-[20px] flex  items-center cursor-pointer rounded-xl border-white hover:border-orange hover:border-opacity-50 border transition-colors hover:bg-orange hover:bg-opacity-5">
+              <Image src={hexagonSvg} alt="hexagon" />
+              <span className="ml-3 ">crosswit</span>
+            </div>
+          </div>
+        </a>
       </Section>
 
       <Section className="mt-20" title="Latest Articles">
