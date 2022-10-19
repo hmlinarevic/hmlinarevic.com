c0935995843c4e2eb6fef08bb70a051256f28736
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-19 23:42:12 +0200

    feat: add page

---
+++ pages/projects.js
@@ -0,0 +1,46 @@
+import Section from '../components/section'
+
+const Tag = ({ children }) => {
+  return (
+    <span className="mr-2 rounded-3xl bg-snow bg-opacity-100 py-1 px-4 text-sm text-black">
+      {children}
+    </span>
+  )
+}
+
+export default function Projects() {
+  return (
+    <>
+      <Section title="Projects">
+        <p className="text-lg">
+          <span className="font-bold">Crosswit</span> - word search & memory
+          trainer.
+        </p>
+        {/* tags */}
+        <div className="mt-2">
+          <Tag>game</Tag>
+          <Tag>puzzle</Tag>
+        </div>
+        <p className="mt-6 text-lg">
+          Being a hybrid between a memory game and a word search puzzle crosswit
+          is designed around the idea of improving cognitive functioning.
+        </p>
+        <p className="mt-6 text-lg">
+          Two aspects of the game - memorizing and searching for words - could
+          improve your short term memory, focus and concentration.
+        </p>
+        <p className="mt-6 text-lg">
+          Try it out,{' '}
+          <a
+            className="underline decoration-orange decoration-2 underline-offset-2 hover:bg-orange hover:text-white hover:no-underline"
+            href="https://www.crosswit.io"
+            target="_blank"
+          >
+            crosswit.io
+          </a>
+          .
+        </p>
+      </Section>
+    </>
+  )
+}
