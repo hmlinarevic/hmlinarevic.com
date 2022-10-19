bd8d85f84e97c5c051c6410429c8f79285dc3f0c
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-19 23:44:00 +0200

    feat: add projects section

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import Section from '../components/section'
-import Hero from '../components/hero'
-import Text from '../components/text'
+import { useState } from 'react'
+
+import ReactMarkdown from 'react-markdown'
+
+import Section from '../components/layout/section'
+import Hero from '../components/layout/hero'
 import ArticleList from '../components/article/article-list'
+import ProjectLink from '../components/projects/project-link'
 
 import { getMyData } from '../lib/cms/me'
 import { getSiteData } from '../lib/cms/site'
 import { getArticlesData } from '../lib/cms/articles'
-import Image from 'next/image'
-
-import hexagonSvg from '../public/hexagon.svg'
 
 export default function Home({ me, site, articles }) {
   return (
@@ -17,24 +18,15 @@ export default function Home({ me, site, articles }) {
         <Hero imgUrl={me.imgUrl} />
       </Section>
 
-      <Section className="mt-24" title="A Bit About the Site">
-        <Text className="font-serif text-lg" data={site.about} />
+      <Section title="A Bit About the Site">
+        <ReactMarkdown className="text-lg" children={site.about} />
       </Section>
 
-      <Section className="mt-24" title="Projects">
-        <Text className="font-serif text-lg" />
-
-        <a href="https://www.crosswit.io">
-          <div className="w-[calc(100%+40px)] ml-[-20px]">
-            <div className="mt-10 py-[10px] px-[20px] flex  items-center cursor-pointer rounded-xl border-white hover:border-orange hover:border-opacity-50 border transition-colors hover:bg-orange hover:bg-opacity-5">
-              <Image src={hexagonSvg} alt="hexagon" />
-              <span className="ml-3 ">crosswit</span>
-            </div>
-          </div>
-        </a>
+      <Section title="Projects">
+        <ProjectLink url="https://www.crosswit.io" />
       </Section>
 
-      <Section className="mt-20" title="Latest Articles">
+      <Section title="Latest Articles">
         <ArticleList data={articles} />
       </Section>
     </>
