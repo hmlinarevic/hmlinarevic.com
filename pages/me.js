14b4d05f6bdff70dd134ebccf75859be0a745a9d
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-20 20:04:10 +0200

    refactor: removed intro and modified link colors

--- pages/me.js
+++ pages/me.js
@@ -2,35 +2,25 @@ import Link from 'next/link'
 
 import ReactMarkdown from 'react-markdown'
 
-import Intro from '../components/intro'
-import Section from '../components/section'
+import Section from '../components/layout/section'
 
 import { getMyData } from '../lib/cms/me'
 
 const Me = ({ about }) => {
   return (
-    <>
-      <Intro title="about me">
-        Tall, strong, blonde and beautiful with blue eyes - as one Croatian
-        comedian would say when talking about himself on stage. In reality, he
-        is short and chubby but he's got charisma.
-      </Intro>
-      <hr className="w-[60px] mx-auto mt-[-2.5rem] mb-12" />
+    <Section title="About me">
+      <ReactMarkdown className="text-lg leading-relaxed">{about}</ReactMarkdown>
 
-      <ReactMarkdown className="text-xl leading-relaxed">{about}</ReactMarkdown>
-
-      <p className="text-xl leading-relaxed mt-5">
+      <p className="mt-5 text-lg leading-relaxed">
         If you want to know more, check out this{' '}
         <Link href="/blog/shoulders-of-giants">
-          <a className="underline decoration-[#619B60] underline-offset-2 hover:bg-[#619B60] hover:text-[#fff]">
+          <a className="underline decoration-orange decoration-2 underline-offset-2 hover:bg-orange hover:text-white">
             article
           </a>
         </Link>
         .
       </p>
-
-      <Section className="mt-24" title="Courses"></Section>
-    </>
+    </Section>
   )
 }
 
