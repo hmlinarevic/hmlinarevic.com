2902005ced9a954cf00f4a37d06b16281af872af
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-20 20:12:39 +0200

    refactor: modified styles and date component

--- components/article-item.js
+++ components/article-item.js
@@ -1,22 +1,24 @@
 import Link from 'next/link'
 
-import Date from '../date'
+import { parseISO, format } from 'date-fns'
 
 export default function ArticleItem({ title, publishedAt, slug }) {
   const url = `/blog/${slug}`
+  const date = parseISO(publishedAt)
+
   return (
     <li>
       <Link href={url}>
         <a>
-          <div className="w-[calc(100%+40px)] ml-[-20px]">
-            <div className="py-[10px] px-[20px] flex justify-between items-center cursor-pointer rounded-xl hover:bg-neutral-100">
+          <div className="ml-[-20px] w-[calc(100%+40px)]">
+            <div className="flex cursor-pointer items-center justify-between py-[10px] px-[20px] decoration-night decoration-2 underline-offset-2 hover:underline">
               <div className="flex items-center">
-                <h3 className="text-xl inline mr-4">{title}</h3>
+                <h3 className="mr-4 inline text-xl">{title}</h3>
               </div>
 
               <span className="item-end text-sm text-neutral-500">
                 <code>
-                  <Date dateString={publishedAt} formatString="LLL dd" />
+                  <time dateTime={publishedAt}>{format(date, 'LLL dd')}</time>
                 </code>
               </span>
             </div>
