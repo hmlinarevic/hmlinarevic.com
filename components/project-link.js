8413ea26f4f180c00d1461b9752f56665ea16bca
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-20 20:11:49 +0200

    feat: add component

---
+++ components/project-link.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+import HexagonSvg from './icons/hexagon-svg'
+
+export default function ProjectLink({ url, label }) {
+  const [isUserHovering, setIsUserHovering] = useState(false)
+
+  const handleUserHovering = () => {
+    setIsUserHovering((prevState) => !prevState)
+  }
+
+  return (
+    <a
+      onMouseEnter={handleUserHovering}
+      onMouseLeave={handleUserHovering}
+      href={url}
+      className="block"
+    >
+      <div className="ml-[-20px] flex w-[calc(100%+40px)] cursor-pointer items-center justify-between rounded-xl py-[10px] px-[20px] transition hover:bg-orange hover:text-white">
+        <div className="flex items-center">
+          <span className={isUserHovering ? 'animate-spin-slow' : ''}>
+            <HexagonSvg />
+          </span>
+          <span className="ml-2 inline text-lg">{label}</span>
+        </div>
+      </div>
+    </a>
+  )
+}
