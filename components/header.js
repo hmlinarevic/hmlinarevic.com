0b97da65de4cc1f632e1e713aefba58d48207196
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-20 15:48:50 +0200

    refactor: move nav directly into component

--- components/header.js
+++ components/header.js
@@ -1,13 +1,36 @@
-import Navigation from './navigation'
-
-const Header = () => {
+export default function Header() {
   return (
-    <header className="h-16 bg-neutral-100 border-b">
-      <div className="w-[720px] h-full m-auto flex items-center">
-        <Navigation />
+    <header className="h-16 border-b bg-neutral-100">
+      <div className="m-auto flex h-full w-[720px] items-center">
+        <nav className="w-full">
+          <ul className="flex items-center justify-between">
+            <Link href="/">
+              <a>
+                <span className="block text-xl font-normal">
+                  <span className="text-[#619B60]">hrvoje</span> mlinarevic
+                </span>
+              </a>
+            </Link>
+            <div className="flex">
+              <li className="mr-10 cursor-pointer rounded-lg p-2 text-[1rem] transition hover:bg-neutral-200">
+                <Link href="/me">
+                  <a>about me</a>
+                </Link>
+              </li>
+              <li className="mr-10 cursor-pointer rounded-lg p-2 text-[1rem] transition hover:bg-neutral-200">
+                <Link href="/blog">
+                  <a>articles</a>
+                </Link>
+              </li>
+              <li className="mr-10 cursor-pointer rounded-lg p-2 text-[1rem] transition hover:bg-neutral-200">
+                <Link href="/projects">
+                  <a>projects</a>
+                </Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
       </div>
     </header>
   )
 }
-
-export default Header
