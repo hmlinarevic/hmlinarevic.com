fe94917553c532c2547b04f6905c8a16a002297d
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-01-31 19:55:14 +0100

    fix: formatting

--- components/article-item.js
+++ components/article-item.js
@@ -9,7 +9,6 @@ export default function ArticleItem({ title, publishedAt, slug }) {
   return (
     <li>
       <Link href={url}>
-
         <div className="ml-[-20px] w-[calc(100%+40px)]">
           <div className="flex cursor-pointer items-center justify-between py-[10px] px-[20px] decoration-night decoration-2 underline-offset-2 hover:underline">
             <div className="flex items-center">
@@ -23,8 +22,7 @@ export default function ArticleItem({ title, publishedAt, slug }) {
             </span>
           </div>
         </div>
-
       </Link>
     </li>
-  );
+  )
 }
