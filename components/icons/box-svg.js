5d660ccde4dc01b88a2a7cf155812b5605d22f49
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-01 12:16:52 +0100

    feat: enable className props

--- components/icons/box-svg.js
+++ components/icons/box-svg.js
@@ -1,13 +1,14 @@
-export default function BoxSvg() {
+export default function BoxSvg({ className }) {
   return (
     <svg
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       width="20"
       height="20"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="1.5"
+      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
