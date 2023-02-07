606dbc3c5e35b769d133f0ca6c6107c0ab261a60
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-07 22:15:41 +0100

    fix: resolve merge conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
 import '../styles/globals.css'
 import '../styles/header.css'
 
