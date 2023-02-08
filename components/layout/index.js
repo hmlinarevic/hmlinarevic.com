16e2bb65358a034435ca06c506532986b094fd3e
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-08 11:19:20 +0100

    fix: remove duplicate code

--- components/layout/index.js
+++ components/layout/index.js
@@ -4,12 +4,6 @@ export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main className="mx-auto max-w-[720px] py-20">{children}</main>
-    </>
-  )
-  return (
-    <>
-      <Header />
 
       <main className="mx-auto min-w-[340px] max-w-[752px] py-10 sm:py-20 px-4">
         {children}
