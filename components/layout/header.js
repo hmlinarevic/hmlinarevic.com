565bc521101cfcf6d38c5ece2975844f8bb04585
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-09 19:51:48 +0100

    feat: modify navigation to use hamburger button

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,86 +1,12 @@
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
 import Container from './container'
+import Navigation from '../navigation'
 
 export default function Header() {
-  const [isToggle, setIsToggle] = useState(false)
-
-  const handleToggle = () => {
-    setIsToggle((prevState) => !prevState)
-  }
-
-  const handleLink = () => {
-    setIsToggle(false)
-  }
-
-  const handleClose = () => {
-    setIsToggle(false)
-  }
-
-  useEffect(() => {
-    if (isToggle) {
-      document.body.style.overflow = 'hidden'
-      document.body.style.height = '100vh'
-    } else {
-      document.body.style.overflow = ''
-      document.body.style.height = 'auto'
-    }
-  }, [isToggle])
-
-  return (
-    <header className="relative h-16 bg-black">
-      <Container>
-        <div className="m-auto flex min-w-[340px] max-w-[720px] h-full items-center">
-          <nav className="w-full text-white">
-            <div className="flex items-center justify-between">
-              <Link href="/">
-                <span className="block text-xl font-normal">
-                  <span className="text-orange">hrvoje</span> mlinarevic
-                </span>
-              </Link>
-              <button
-                className="nav-menu-toggler sm:hidden"
-                onClick={handleToggle}
-              >
-                <div className="nav-menu-toggler-icon" />
-              </button>
-              {/* nav menu */}
-              <div
-                className={`nav-menu bg-black ${
-                  isToggle ? 'nav-menu--active' : ''
-                }`}
-              >
-                <ul>
-                  <li className="mr-10 text-[1rem]" onClick={handleLink}>
-                    <Link href="/me" className="cursor-pointer p-2">
-                      about me
-                    </Link>
-                  </li>
-                  <li className="mr-10 text-[1rem]" onClick={handleLink}>
-                    <Link href="/blog" className="cursor-pointer p-2">
-                      articles
-                    </Link>
-                  </li>
-                  <li className="text-[1rem]" onClick={handleLink}>
-                    <Link href="/projects" className="cursor-pointer p-2">
-                      projects
-                    </Link>
-                  </li>
-                </ul>{' '}
-                {/* nav menu close button */}
-                <button
-                  className={`nav-menu-toggler-close-btn ${
-                    !isToggle ? 'hidden' : ''
-                  }`}
-                  onClick={handleClose}
-                >
-                  <span className="nav-menu-toggler-close" />
-                </button>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </Container>
-    </header>
-  )
+    return (
+        <header className="relative h-16 bg-black">
+            <Container className="min-w-[340px] max-w-[720px] border-red-900 border-2">
+                <Navigation />
+            </Container>
+        </header>
+    )
 }
---
+++ components/navigation.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import Hamburger from './hamburger'
+
+export default function Navigation() {
+    const [isToggle, setIsToggle] = useState(false)
+
+    const handleToggle = () => {
+        setIsToggle((prevState) => !prevState)
+    }
+
+    const handleLink = () => {
+        setIsToggle(false)
+    }
+
+    useEffect(() => {
+        if (isToggle) {
+            document.body.style.overflow = 'hidden'
+            document.body.style.height = '100vh'
+        } else {
+            document.body.style.overflow = ''
+            document.body.style.height = 'auto'
+        }
+    }, [isToggle])
+
+    return (
+        <nav className="h-full flex items-center justify-between">
+            <Link href="/">
+                <span className="block text-xl font-normal">
+                    <span className="text-orange">hrvoje</span> mlinarevic
+                </span>
+            </Link>
+            <Hamburger onToggle={handleToggle} className="sm:hidden" />
+            <ul
+                className={`nav-menu bg-black ${
+                    isToggle ? 'nav-menu--active' : ''
+                }`}
+            >
+                <li className="mr-10 text-[1rem]" onClick={handleLink}>
+                    <Link href="/me" className="cursor-pointer p-2">
+                        about me
+                    </Link>
+                </li>
+                <li className="mr-10 text-[1rem]" onClick={handleLink}>
+                    <Link href="/blog" className="cursor-pointer p-2">
+                        articles
+                    </Link>
+                </li>
+                <li className="text-[1rem]" onClick={handleLink}>
+                    <Link href="/projects" className="cursor-pointer p-2">
+                        projects
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
