263c81aa9aa9304aba052d3254b2a291bb8285e5
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-13 23:05:32 +0100

    fix: height on project image on smaller devices

--- components/project/project-details.js
+++ components/project/project-details.js
@@ -3,46 +3,52 @@ import { nanoid } from 'nanoid'
 import Tag from './tag'
 
 export default function ProjectDetails({
-  className,
-  img,
-  projectName,
-  tagline,
-  tags,
-  descriptions,
-  link,
+    className,
+    img,
+    projectName,
+    tagline,
+    tags,
+    descriptions,
+    link,
 }) {
-  return (
-    <div className={className}>
-      <div className="text-lg flex">
-        <Image src={img.src} alt={img.alt} height={32} className="mr-2" />
-        <span>
-          <span className="font-bold">{projectName}</span>
-          <span> - {tagline}</span>
-        </span>
-      </div>
-      {/* tags */}
-      <div className="mt-2">
-        {tags.map((tag) => (
-          <Tag key={nanoid()}>{tag}</Tag>
-        ))}
-      </div>
-      {descriptions.map((desc) => (
-        <p key={nanoid()} className="mt-6 text-lg">
-          {desc}.
-        </p>
-      ))}
-      <p className="mt-6 text-lg">
-        Try it out,{' '}
-        <a
-          className="underline decoration-orange decoration-2 underline-offset-2 hover:bg-orange hover:text-white hover:no-underline"
-          href={link.href}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {link.name}
-        </a>
-        .
-      </p>
-    </div>
-  )
+    return (
+        <div className={className}>
+            <div className="text-lg flex">
+                <Image
+                    src={img.src}
+                    alt={img.alt}
+                    height={32}
+                    className="mr-2"
+                    style={{ height: '100%' }}
+                />
+                <span>
+                    <span className="font-bold">{projectName}</span>
+                    <span> - {tagline}</span>
+                </span>
+            </div>
+            {/* tags */}
+            <div className="mt-2">
+                {tags.map((tag) => (
+                    <Tag key={nanoid()}>{tag}</Tag>
+                ))}
+            </div>
+            {descriptions.map((desc) => (
+                <p key={nanoid()} className="mt-6 text-lg">
+                    {desc}.
+                </p>
+            ))}
+            <p className="mt-6 text-lg">
+                Try it out,{' '}
+                <a
+                    className="underline decoration-orange decoration-2 underline-offset-2 hover:bg-orange hover:text-white hover:no-underline"
+                    href={link.href}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    {link.name}
+                </a>
+                .
+            </p>
+        </div>
+    )
 }
