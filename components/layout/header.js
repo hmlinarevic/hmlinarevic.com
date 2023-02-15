b7bf8992779569e951bfbe57c4f18ab15acaeefc
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-15 22:11:04 +0100

    feat: mobile nav menu slides from the top

--- components/layout/header.js
+++ components/layout/header.js
@@ -2,11 +2,11 @@ import Container from './container'
 import Navigation from '../navigation'
 
 export default function Header() {
-    return (
-        <header className="relative h-16 bg-black">
-            <Container className="min-w-[340px] max-w-[720px]">
-                <Navigation />
-            </Container>
-        </header>
-    )
+  return (
+    <header className="relative h-16 bg-black border-b border-dark">
+      <Container className="min-w-[340px] max-w-[720px]">
+        <Navigation />
+      </Container>
+    </header>
+  )
 }
