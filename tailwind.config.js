2f81f764179306fc5595c90ef79ac93e47d593dc
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-16 23:26:31 +0100

    feat: add orange color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                dark: '#22272f',
-                black: '#2e3440',
-                night: '#4c566a',
-                white: '#eceff4',
-                orange: '#d08770',
-            },
-            animation: {
-                'spin-slow': 'spin 3s linear infinite',
-            },
-        },
-
-        fontFamily: {
-            sans: ['Inter', 'sans-serif'],
-            serif: ['IBM Plex Serif', 'serif'],
-        },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#22272f',
+        black: '#2e3440',
+        night: '#4c566a',
+        white: '#eceff4',
+        orange: '#d08770',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
     },
-    plugins: [],
+
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['IBM Plex Serif', 'serif'],
+    },
+  },
+  plugins: [],
 }
