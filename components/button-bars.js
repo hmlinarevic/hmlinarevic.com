9ac8eec49c22653727513dd6de91e7c4435793fe
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-02-20 20:54:12 +0100

    feat: move local state to parent

--- components/button-bars.js
+++ components/button-bars.js
@@ -1,25 +1,20 @@
-import { useState } from 'react'
-
-export default function ButtonBars({ onToggle }) {
-  const [addStyles, setAddStyles] = useState(false)
-
+export default function ButtonBars({ isToggle, onToggle }) {
   const handleButtonClick = () => {
-    setAddStyles((prevState) => !prevState)
     onToggle()
   }
 
   return (
     <button onClick={handleButtonClick} className="p-2 sm:hidden">
       <span
-        style={addStyles ? { background: '#d08770' } : null}
+        style={isToggle ? { background: '#d08770' } : null}
         className="block h-[2px] w-8 bg-white transition-colors"
       ></span>
       <span
-        style={addStyles ? { background: '#d08770' } : null}
+        style={isToggle ? { background: '#d08770' } : null}
         className="block h-[2px] w-8 bg-white my-1 transition-colors"
       ></span>
       <span
-        style={addStyles ? { background: '#d08770' } : null}
+        style={isToggle ? { background: '#d08770' } : null}
         className="block h-[2px] w-8 bg-white transition-colors"
       ></span>
     </button>
